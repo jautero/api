d87c81addda5b7b5fee1bb80f5b5c1699876891b
Juha Autero <jautero@iki.fi>
2016-10-19 06:19:50 +0300

    Added function for creating logged-in agents.

--- integration-tests/test/test.js
+++ integration-tests/test/test.js
@@ -7,9 +7,22 @@ var cert = fs.readFileSync('../nginx/ssl/localhost.cert','utf8');
 var memberlist = [];
 var memberstats = {"":{"total":0}};
 // Create agent for unlogged and admin sessions
-var unlogged=request.agent(host,{ca:cert});
-var admin=request.agent(host,{ca:cert});
+var defaultagent=request.agent(host,{ca:cert});
 var loginparams={email:"admin@example.com",key:"key"};
+
+function getTestAgent(login) {
+    var agent=request.agent(host,{ca:cert});
+    if (login) {
+        it("try to login",function (done) {
+            agent.get("/api/kansa/login")
+                .query(loginparams)
+                .expect("set-cookie",/w75/)
+                .expect(200,{status:'success', email:loginparams["email"]})
+                .end(done);
+        });
+    }
+    return agent;
+}
 
 describe("Check that API services are up",function () {
     this.timeout(120000)
@@ -22,20 +35,22 @@ describe("Check that API services are up",function () {
         }
     })
     it("Should respond with json on api/kansa/", function (done) {
-        unlogged.get("/api/kansa/")
+        defaultagent.get("/api/kansa/")
             .expect("Content-Type",/json/)
         .end(done);
     })
     it("Should respond with json on api/hugo/", function (done) {
-        unlogged.get("/api/hugo/")
+        defaultagent.get("/api/hugo/")
             .expect("Content-Type",/json/)
         .end(done);
     })
-})
+});
+
+
 
 describe("Member list",function () {
     it ("Returns 'success' as status and test data member list.", function (done) {
-        unlogged.get("/api/kansa/public/people")
+        defaultagent.get("/api/kansa/public/people")
         .expect(200,{status: 'success', data: memberlist})
             .end(done);
         })
@@ -43,48 +58,42 @@ describe("Member list",function () {
 
 describe("Country statistics", function () {
     it("Returns country statistics", function (done) {
-        unlogged.get("/api/kansa/public/stats").expect(200,{status:'success', members: memberstats}).end(done)
+        defaultagent.get("/api/kansa/public/stats").expect(200,{status:'success', members: memberstats}).end(done)
     })
 })
 
 describe("Login",function () {
     context("Successful login",function () {
-        it("gets a session cookie or it gets the hose again.",function (done) {
-            admin.get("/api/kansa/login")
-                .query(loginparams)
-                .expect("set-cookie",/w75/)
-                .expect(200,{status:'success', email:loginparams["email"]})
-            .end(done);
-        });
+        var testagent=getTestAgent(true);
         it("gets user information",function (done) {
-            admin.get("/api/kansa/user")
+            testagent.get("/api/kansa/user")
                 .expect(200)
                 .end(done);
         })
     });
     context("Login with wrong email",function () {
         it("gets 401 response",function (done) {
-            unlogged.get("/api/kansa/login")
+            defaultagent.get("/api/kansa/login")
                 .query({email:"foo@doo.com",key:loginparams["key"]})
                 .expect(401)
                 .end(done)
         });
         it("gets unauthorized from /api/kansa/usr",function (done) {
-            unlogged.get("/api/kansa/user")
+            defaultagent.get("/api/kansa/user")
                 .expect(401,{status:"unauthorized"})
                 .end(done)
         })
     })
     context("Login with wrong key",function () {
         it("gets 401 response",function (done) {
-            unlogged.get("/api/kansa/login")
+            defaultagent.get("/api/kansa/login")
                 .query({email:loginparams["email"],key:"foo"})
                 .expect(401)
                 .end(done)
         })
         
         it("gets unauthorized from /api/kansa/usr",function (done) {
-            unlogged.get("/api/kansa/user")
+            defaultagent.get("/api/kansa/user")
                 .expect(401,{status:"unauthorized"})
                 .end(done)
         })
@@ -94,14 +103,7 @@ describe("Login",function () {
 })
 
 describe("Logout",function() {
-    var testagent = request.agent(host,{ca:cert});
-    before(function (done) {
-        testagent.get("/api/kansa/login")
-            .query(loginparams)
-            .expect("set-cookie",/w75/)
-            .expect(200,{status:'success',email:loginparams["email"]})
-            .end(done)
-    });
+    var testagent = getTestAgent(true);
     context("Successfull logout",function () {
         it("should be successfull",function (done) {
             testagent.get("/api/kansa/logout")
@@ -117,12 +119,12 @@ describe("Logout",function() {
     });
     context("Not logged in", function () {
         it("logout should be unauthorized", function (done) {
-            unlogged.get("/api/kansa/logout")
+            defaultagent.get("/api/kansa/logout")
                 .expect(401,{status:"unauthorized"})
                 .end(done);
         });
         it ("gets unauthorized from /api/kansa/user",function (done) {
-            testagent.get("/api/kansa/user")
+            defaultagent.get("/api/kansa/user")
                 .expect(401,{status:"unauthorized"})
                 .end(done);
         });
